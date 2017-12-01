9e62b6e1d63d78d5f82ab625f1de15486ef88675
Karl Entwistle <karl@entwistle.com>
2017-12-01 23:11:06 +0000

    TVL pack
    8-bit type followed by an 8-bit length followed by a variable length value format (TLV8).
    
    - Values less than or equal to 255 bytes must be contained in a single TLV item and not fragmented.
    - Values larger than 255 bytes must be fragmented into multiple, TLV fragment items.
    - Each TLV fragment item must begin with an 8-bit type followed by an 8-bit length.
    - Each TLV fragment item's type must be same.
    - Each TLV fragment item's length must describe only that fragment's portion of the value.
    - TLV fragment items must be contiguous.
    - Only the last TLV fragment item in series of contiguous TLV fragment items may have non-255 byte length.
    - There may be multiple, separate TLV items of the same type if separated by a TLV item of a different type.

--- lib/rubyhome/tlv.rb
+++ lib/rubyhome/tlv.rb
@@ -1,27 +1,58 @@
 module Rubyhome
   module TLV
     INTEGER_UNPACKER = ->(value) { [value].pack('H*').unpack('C*').first }
+    INTEGER_PACKER = ->(value) { [value].pack('C*').unpack('H*').first }
+
     UTF8_UNPACKER = ->(value) { [value.force_encoding("UTF-8")].pack('H*') }
+    UTF8_PACKER = ->(value) { value.force_encoding("UTF-8").bytes.map { |b| b.to_s(16) }.join }
+
     BYTES_UNPACKER = ->(value) { value }
-    TLV = Struct.new(:type, :name, :unpack)
+    BYTES_PACKER = ->(value) { value }
+
+    TLV = Struct.new(:type, :name, :unpack, :pack)
     TLVs = [
-      TLV.new("00", 'kTLVType_Method', INTEGER_UNPACKER),
-      TLV.new("01", 'kTLVType_Identifier', UTF8_UNPACKER),
-      TLV.new("02", 'kTLVType_Salt', BYTES_UNPACKER),
-      TLV.new("03", 'kTLVType_PublicKey', BYTES_UNPACKER),
-      TLV.new("04", 'kTLVType_Proof', BYTES_UNPACKER),
-      TLV.new("05", 'kTLVType_EncryptedData', BYTES_UNPACKER),
-      TLV.new("06", 'kTLVType_State', INTEGER_UNPACKER),
-      TLV.new("07", 'kTLVType_Error', INTEGER_UNPACKER),
-      TLV.new("08", 'kTLVType_RetryDelay', INTEGER_UNPACKER),
-      TLV.new("09", 'kTLVType_Certificate', BYTES_UNPACKER),
-      TLV.new("0a", 'kTLVType_Signature', BYTES_UNPACKER),
-      TLV.new("0b", 'kTLVType_Permissions', INTEGER_UNPACKER),
-      TLV.new("0c", 'kTLVType_FragmentData', BYTES_UNPACKER),
-      TLV.new("0d", 'kTLVType_FragmentLast', BYTES_UNPACKER),
+      TLV.new("00", 'kTLVType_Method', INTEGER_UNPACKER, INTEGER_PACKER),
+      TLV.new("01", 'kTLVType_Identifier', UTF8_UNPACKER, UTF8_PACKER),
+      TLV.new("02", 'kTLVType_Salt', BYTES_UNPACKER, BYTES_PACKER),
+      TLV.new("03", 'kTLVType_PublicKey', BYTES_UNPACKER, BYTES_PACKER),
+      TLV.new("04", 'kTLVType_Proof', BYTES_UNPACKER, BYTES_PACKER),
+      TLV.new("05", 'kTLVType_EncryptedData', BYTES_UNPACKER, BYTES_PACKER),
+      TLV.new("06", 'kTLVType_State', INTEGER_UNPACKER, INTEGER_PACKER),
+      TLV.new("07", 'kTLVType_Error', INTEGER_UNPACKER, INTEGER_PACKER),
+      TLV.new("08", 'kTLVType_RetryDelay', INTEGER_UNPACKER, INTEGER_PACKER),
+      TLV.new("09", 'kTLVType_Certificate', BYTES_UNPACKER, BYTES_PACKER),
+      TLV.new("0a", 'kTLVType_Signature', BYTES_UNPACKER, BYTES_PACKER),
+      TLV.new("0b", 'kTLVType_Permissions', INTEGER_UNPACKER, INTEGER_PACKER),
+      TLV.new("0c", 'kTLVType_FragmentData', BYTES_UNPACKER, BYTES_PACKER),
+      TLV.new("0d", 'kTLVType_FragmentLast', BYTES_UNPACKER, BYTES_PACKER),
     ].freeze
 
     class << self
+      def pack(hash)
+        data = ""
+
+        pack_objects(hash).each do |type, value|
+          value.chars.each_slice(510).map(&:join).each do |value_slice|
+            length = INTEGER_PACKER.call([value_slice].pack('H*').length)
+
+            data << type
+            data << length
+            data << value_slice
+          end
+        end
+
+        [data].pack('H*')
+      end
+
+      def pack_objects(objects)
+        objects.each_with_object({}) do |(unpacked_key, unpacked_value), memo|
+          tlv_value = TLVs.find { |tlv| tlv.name == unpacked_key }
+          packed_key = tlv_value.type
+          packed_value = tlv_value.pack.call(unpacked_value)
+          memo[packed_key] = packed_value
+        end
+      end
+
       def unpack(input)
         data = input.unpack('H*')[0]
         objects = {}
--- spec/lib/tlv_spec.rb
+++ spec/lib/tlv_spec.rb
@@ -149,6 +149,156 @@ RSpec.describe Rubyhome::TLV do
     end
   end
 
+  describe '.pack' do
+    subject { described_class.pack(input) }
+
+    context 'kTLVType_Method' do
+      let(:input) { {'kTLVType_Method' => 255} }
+      it { is_expected.to eql(["0001ff"].pack('H*')) }
+    end
+
+    context 'kTLVType_Identifier' do
+      let(:input) { {'kTLVType_Identifier' => 'hello'} }
+      it { is_expected.to eql(["010568656c6c6f"].pack('H*')) }
+    end
+
+    context 'kTLVType_Salt' do
+      let(:input) { {'kTLVType_Salt' => 'c8eb453673a905cc5ca6b64ce85e22e9'} }
+      it { is_expected.to eql(["0210c8eb453673a905cc5ca6b64ce85e22e9"].pack('H*')) }
+    end
+
+    context 'kTLVType_PublicKey' do
+      let(:input) { {'kTLVType_PublicKey' => '596ffa328760d19e7b9d'} }
+      it { is_expected.to eql(["030a596ffa328760d19e7b9d"].pack('H*')) }
+    end
+
+    context 'kTLVType_Proof' do
+      let(:input) { {'kTLVType_Proof' => '0fab'} }
+      it { is_expected.to eql(["04020fab"].pack('H*')) }
+    end
+
+    context 'kTLVType_EncryptedData' do
+      let(:input) { {'kTLVType_EncryptedData' => '49625160'} }
+      it { is_expected.to eql(["050449625160"].pack('H*')) }
+    end
+
+    context 'kTLVType_State' do
+      let(:input) { {'kTLVType_State' => 0} }
+      it { is_expected.to eql(["060100"].pack('H*')) }
+    end
+
+    context 'kTLVType_Error' do
+      let(:input) { {'kTLVType_Error' => 15} }
+      it { is_expected.to eql(["07010f"].pack('H*')) }
+    end
+
+    context 'kTLVType_RetryDelay' do
+      let(:input) { {'kTLVType_RetryDelay' => 170} }
+      it { is_expected.to eql(["0801aa"].pack('H*')) }
+    end
+
+    context 'kTLVType_Certificate' do
+      let(:input) { {'kTLVType_Certificate' => 'c8eb453673a905cc5ca6b64ce85e22e9'} }
+      it { is_expected.to eql(["0910c8eb453673a905cc5ca6b64ce85e22e9"].pack('H*')) }
+    end
+
+    context 'kTLVType_Signature' do
+      let(:input) { {'kTLVType_Signature' => 'c8eb453673a905cc5ca6b64ce85e22e9'} }
+      it { is_expected.to eql(["0a10c8eb453673a905cc5ca6b64ce85e22e9"].pack('H*')) }
+    end
+
+    context 'kTLVType_Permissions' do
+      let(:input) { {'kTLVType_Permissions' => 18} }
+      it { is_expected.to eql(["0B0112"].pack('H*')) }
+    end
+
+    context 'kTLVType_FragmentData' do
+      let(:input) { {'kTLVType_FragmentData' => 'c8eb453673a905cc5ca6b64ce85e22e9'} }
+      it { is_expected.to eql(["0c10c8eb453673a905cc5ca6b64ce85e22e9"].pack('H*')) }
+    end
+
+    context 'kTLVType_FragmentLast' do
+      let(:input) { {'kTLVType_FragmentLast' => 'c8eb453673a905cc5ca6b64ce85e22e9'} }
+      it { is_expected.to eql(["0d10c8eb453673a905cc5ca6b64ce85e22e9"].pack('H*')) }
+    end
+
+    context '2 small TLVs Integer and Integer' do
+      let(:input) { {'kTLVType_Method' => 255, 'kTLVType_Error' => 15} }
+      it { is_expected.to eql(["0001ff07010f"].pack('H*')) }
+    end
+
+    context '2 small TLVs Integer and ASCII' do
+      let(:input) { {'kTLVType_State' => 3, 'kTLVType_Identifier' => 'hello'} }
+      it { is_expected.to eql(["060103010568656c6c6f"].pack('H*')) }
+    end
+
+    context '1 small TLV, 1 300-byte value split into 2 TLVs, 1 small TLV' do
+      let(:input) do
+        {
+          'kTLVType_Error' => 3,
+          'kTLVType_Signature' => '61' * 300,
+          'kTLVType_Identifier' => 'hello'
+        }
+      end
+      it do
+        is_expected.to eql([
+          "0701030aff616161616161616161616161616161616161616161616161616161616161"\
+          "6161616161616161616161616161616161616161616161616161616161616161616161"\
+          "6161616161616161616161616161616161616161616161616161616161616161616161"\
+          "6161616161616161616161616161616161616161616161616161616161616161616161"\
+          "6161616161616161616161616161616161616161616161616161616161616161616161"\
+          "6161616161616161616161616161616161616161616161616161616161616161616161"\
+          "6161616161616161616161616161616161616161616161616161616161616161616161"\
+          "6161616161616161616161616161610a2d616161616161616161616161616161616161"\
+          "616161616161616161616161616161616161616161616161616161010568656c6c6f"
+        ].pack('H*'))
+      end
+    end
+
+    context 'SRP Start Request' do
+      let(:input) { {'kTLVType_Method' => 0, 'kTLVType_State' => 1} }
+      it { is_expected.to eql(["000100060101"].pack('H*')) }
+    end
+
+    context 'SRP Start Response' do
+      let(:input) do
+        {
+          'kTLVType_Salt' => 'eef0f9f30a085068aa9b129bde03ab39',
+          'kTLVType_PublicKey' =>
+          "90cd46e05e86acd8fa07d1961d37eb594798a31ee1e0e9c76720ab3373bb386a4c869a"\
+          "b7fb84db3012c6ba2edbe2b72a15d491c2c6694ab93a01bd46e3a3165f622484ca4119"\
+          "15d2f1415a730f68b1941168fbc1c10325ad1ee2fb19f770270f9c74e8f35c0bf20403"\
+          "1d665fee200e4ca77d965553388f58e50a5663f25bb40ee487fbee230d1231eaeaf9ad"\
+          "a4ec9c5a7af6b1a8d3167205902961629aec11cadd3cb8c703cb2fa091be7243315677"\
+          "729ecf4b76397dde66a9e930e373bee12637b5aeb347130f00fe915bd6b36abc0212fe"\
+          "734ab913339558f9e24a6f14608763b544e2e5cfae56d0e2fbf516c92002ddb62099a0"\
+          "48df252b39a31c1e105fd60bbc58357ac18cfaff46da6729c1cead884a47c7068408b4"\
+          "824b9aa43b44aedc04cfdfe9cb3997dd8870eb89994cab50ee37eb24d3f6e6b1ba39c2"\
+          "f1b8c967851fe60ac18bbd4f8f14825b45aac1ec31a507d77f077cbc02ebb90d7d8e75"\
+          "2dfb2493ced237bb13992fee996b4e04a81d68d2e9282bba497f7dcba1bab4f3f2a6",
+          'kTLVType_State' => 2,
+        }
+      end
+      it do
+        is_expected.to include(['060102'].pack('H*'))
+        is_expected.to include(['0210eef0f9f30a085068aa9b129bde03ab39'].pack('H*'))
+        is_expected.to include([
+          '03ff90cd46e05e86acd8fa07d1961d37eb594798a31ee1e0e9c76720ab3373bb386a4c',
+          '869ab7fb84db3012c6ba2edbe2b72a15d491c2c6694ab93a01bd46e3a3165f622484ca',
+          '411915d2f1415a730f68b1941168fbc1c10325ad1ee2fb19f770270f9c74e8f35c0bf2',
+          '04031d665fee200e4ca77d965553388f58e50a5663f25bb40ee487fbee230d1231eaea',
+          'f9ada4ec9c5a7af6b1a8d3167205902961629aec11cadd3cb8c703cb2fa091be724331',
+          '5677729ecf4b76397dde66a9e930e373bee12637b5aeb347130f00fe915bd6b36abc02',
+          '12fe734ab913339558f9e24a6f14608763b544e2e5cfae56d0e2fbf516c92002ddb620',
+          '99a048df252b39a31c1e105f'
+        ].pack('H*'))
+        is_expected.to include([
+          '0381d60bbc58357ac18cfaff46da6729c1cead884a47c7068408b4824b9aa43b44aedc',
+          '04cfdfe9cb3997dd8870eb89994cab50ee37eb24d3f6e6b1ba39c2f1b8c967851fe60a',
+          'c18bbd4f8f14825b45aac1ec31a507d77f077cbc02ebb90d7d8e752dfb2493ced237bb',
+          '13992fee996b4e04a81d68d2e9282bba497f7dcba1bab4f3f2a6'
+        ].pack('H*'))
+      end
     end
   end
 end
