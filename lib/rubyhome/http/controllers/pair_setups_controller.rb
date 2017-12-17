bee9162e59cdf48f18b423fdd2a850ad2d84e7ac
Karl Entwistle <karl@entwistle.com>
2017-12-17 18:16:18 +0000

    iOS Device -> Accessory -- `SRP Verify Request'
    Can successfully compute M1

--- lib/rubyhome/http/controllers/pair_setups_controller.rb
+++ lib/rubyhome/http/controllers/pair_setups_controller.rb
@@ -1,43 +1,71 @@
 require_relative '../../srp'
 require_relative '../../tlv'
+require 'byebug'
 
 module Rubyhome
   module HTTP
     class PairSetupsController
-
       def initialize(request)
         @request = request
       end
 
       def create
-        request.body.rewind
-
-        username = 'Pair-Setup'
-        password = '101-48-005'
-        prime_length = 3072
-
-        auth = Rubyhome::SRP::Verifier.new.generate_userauth(username, password)
-
-        # Server retrieves user's verifier and salt from the database.
-        verifier = auth[:verifier]
-        salt = auth[:salt]
-
-        # Server generates challenge for the client.
-        srp = SRP::Verifier.new
-
-        b = srp.generate_B(verifier)
-
-        TLV.pack({
-          'kTLVType_Salt' => salt,
-          'kTLVType_PublicKey' => b,
-          'kTLVType_State' => 2
-        })
+        case unpack_request['kTLVType_State']
+        when 1
+          srp_start_response
+        when 3
+          srp_verify_response
+        end
       end
 
       private
 
       attr_reader :request
 
+      def srp_start_response
+        username = 'Pair-Setup'
+        password = '031-45-154'
+
+        srp = Rubyhome::SRP::Verifier.new
+        auth = srp.generate_userauth(username, password)
+
+        verifier = auth[:verifier]
+        salt = auth[:salt]
+
+        challenge_and_proof = srp.get_challenge_and_proof(username, verifier, salt)
+
+        data = Marshal.dump(challenge_and_proof[:proof])
+        File.open('/Users/karl/gems/rubyhome/bla', 'w') { |file| file.write(data) }
+
+        TLV.pack({
+          'kTLVType_Salt' => challenge_and_proof[:challenge][:salt],
+          'kTLVType_PublicKey' => challenge_and_proof[:challenge][:B],
+          'kTLVType_State' => 2
+        })
+      end
+
+      def srp_verify_response
+        file = File.read('/Users/karl/gems/rubyhome/bla')
+        data = Marshal.load(file)
+
+        srp = Rubyhome::SRP::Verifier.new
+
+        proof = data
+        proof[:A] = unpack_request['kTLVType_PublicKey']
+
+        srp.verify_session(proof, unpack_request['kTLVType_Proof'])
+
+        TLV.pack({
+          'kTLVType_State' => 4
+        })
+      end
+
+      def unpack_request
+        @_unpack_request ||= begin
+          request.body.rewind
+          TLV.unpack(request.body.read)
+        end
+      end
     end
   end
 end
--- spec/http/requests/pair_setup_spec.rb
+++ spec/http/requests/pair_setup_spec.rb
@@ -3,40 +3,58 @@ require_relative '../../../lib/rubyhome/tlv'
 require 'spec_helper'
 
 RSpec.describe "POST /pair-setup" do
-  it "headers contains application/pairing+tlv8 header" do
-    post '/pair-setup', "\x00\x01\x00\x06\x01\x01"
-    expect(last_response.headers).to include('Content-Type' => 'application/pairing+tlv8')
+  context "SRP Start Response" do
+    before do
+      path = File.expand_path("../../../fixtures/srp_start_request", __FILE__)
+      data = File.read(path)
+      post '/pair-setup', data
+    end
+
+    it "headers contains application/pairing+tlv8 header" do
+      expect(last_response.headers).to include('Content-Type' => 'application/pairing+tlv8')
+    end
+
+    it "body contains kTLVType_State" do
+      expect(unpacked_body).to include('kTLVType_State' => 2)
+    end
+
+    it "body contains kTLVType_Salt" do
+      expect(unpacked_body).to include('kTLVType_Salt' => a_kind_of(String))
+    end
+
+    it "body contains kTLVType_Salt at least 31 in length" do
+      salt = unpacked_body['kTLVType_Salt']
+      expect(salt.length).to be >= 31
+    end
+
+    it "body contains kTLVType_PublicKey" do
+      expect(unpacked_body).to include('kTLVType_PublicKey' => a_kind_of(String))
+    end
+
+    it "body contains kTLVType_PublicKey at least 766 in length" do
+      public_key = unpacked_body['kTLVType_PublicKey']
+      expect(public_key.length).to be >= 766
+    end
   end
 
-  it "body contains kTLVType_State" do
-    post '/pair-setup', "\x00\x01\x00\x06\x01\x01"
-    expect(unpacked_body).to include('kTLVType_State' => 2)
-  end
+  context "SRP Verify Response" do
+    before do
+      path = File.expand_path("../../../fixtures/srp_verify_request", __FILE__)
+      data = File.read(path)
+      post '/pair-setup', data, { "CONTENT_TYPE" => "application/pairing+tlv8" }
+    end
 
-  it "body contains kTLVType_Salt" do
-    post '/pair-setup', "\x00\x01\x00\x06\x01\x01"
-    expect(unpacked_body).to include('kTLVType_Salt' => a_kind_of(String))
-  end
+    it "headers contains application/pairing+tlv8 header" do
+      expect(last_response.headers).to include('Content-Type' => 'application/pairing+tlv8')
+    end
 
-  it "body contains kTLVType_Salt at least 31 in length" do
-    post '/pair-setup', "\x00\x01\x00\x06\x01\x01"
-    salt = unpacked_body['kTLVType_Salt']
-    expect(salt.length).to be >= 31
-  end
+    it "body contains kTLVType_State" do
+      expect(unpacked_body).to include('kTLVType_State' => 4)
+    end
 
-  it "body contains kTLVType_PublicKey" do
-    post '/pair-setup', "\x00\x01\x00\x06\x01\x01"
-    expect(unpacked_body).to include('kTLVType_PublicKey' => a_kind_of(String))
-  end
-
-  it "body contains kTLVType_PublicKey at least 766 in length" do
-    post '/pair-setup', "\x00\x01\x00\x06\x01\x01"
-    public_key = unpacked_body['kTLVType_PublicKey']
-    expect(public_key.length).to be >= 766
   end
 
   def unpacked_body
     @_unpacked_body ||= Rubyhome::TLV.unpack(last_response.body)
   end
 end
-
