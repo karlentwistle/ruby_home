116ef27719cedefe68a11a2fee684c3dbac9d12f
Karl Entwistle <karl@entwistle.com>
2018-02-17 16:48:55 +0000

    Add put characters endpoint

--- lib/rubyhome/http/controllers/characteristics_controller.rb
+++ lib/rubyhome/http/controllers/characteristics_controller.rb
@@ -11,6 +11,15 @@ module Rubyhome
           JSON.generate({"status" => -70401})
         end
       end
+
+      put '/characteristics' do
+        content_type 'application/hap+json'
+
+        unless cache[:controller_to_accessory_key] && cache[:accessory_to_controller_key]
+          status 401
+          JSON.generate({"status" => -70401})
+        end
+      end
     end
   end
 end
--- spec/http/requests/characteristics_spec.rb
+++ spec/http/requests/characteristics_spec.rb
@@ -1,21 +1,43 @@
 require 'spec_helper'
 
-RSpec.describe 'GET /characteristics' do
-  context 'Request denied due to insufficient privileges' do
-    before do
-      get '/characteristics', nil, {'CONTENT_TYPE' => 'application/hap+json'}
-    end
+RSpec.describe '/characteristics' do
+  context 'GET' do
+    context 'Request denied due to insufficient privileges' do
+      before do
+        get '/characteristics', nil, {'CONTENT_TYPE' => 'application/hap+json'}
+      end
 
-    it 'headers contains application/hap+json' do
-      expect(last_response.headers).to include('Content-Type' => 'application/hap+json')
-    end
+      it 'headers contains application/hap+json' do
+        expect(last_response.headers).to include('Content-Type' => 'application/hap+json')
+      end
 
-    it 'response status 401' do
-      expect(last_response.status).to eql(401)
-    end
+      it 'response status 401' do
+        expect(last_response.status).to eql(401)
+      end
 
-    it 'response body includes status' do
-      expect(last_response.body).to eql('{"status":-70401}')
+      it 'response body includes status' do
+        expect(last_response.body).to eql('{"status":-70401}')
+      end
+    end
+  end
+
+  context 'PUT' do
+    context 'Request denied due to insufficient privileges' do
+      before do
+        put '/characteristics', nil, {'CONTENT_TYPE' => 'application/hap+json'}
+      end
+
+      it 'headers contains application/hap+json' do
+        expect(last_response.headers).to include('Content-Type' => 'application/hap+json')
+      end
+
+      it 'response status 401' do
+        expect(last_response.status).to eql(401)
+      end
+
+      it 'response body includes status' do
+        expect(last_response.body).to eql('{"status":-70401}')
+      end
     end
   end
 end
