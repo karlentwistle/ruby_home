f252e484cb8583a42bfd7663d75d54ea97fc2fa4
Karl Entwistle <karl@entwistle.com>
2018-03-03 14:09:45 +0000

    Add "hardcoded" accessory json file
    This will need to be dynamic soon

--- lib/rubyhome/http/controllers/accessories_controller.rb
+++ lib/rubyhome/http/controllers/accessories_controller.rb
@@ -5,8 +5,14 @@ module Rubyhome
     class AccessoriesController < ApplicationController
       get '/accessories' do
         content_type 'application/hap+json'
-        status 401
-        JSON.generate({"status" => -70401})
+
+        if cache[:controller_to_accessory_key] && cache[:accessory_to_controller_key]
+          path = File.expand_path('../../public/example_accessory.json', __FILE__)
+          data = File.read(path)
+        else
+          status 401
+          JSON.generate({"status" => -70401})
+        end
       end
     end
   end
--- spec/http/requests/accessories_spec.rb
+++ spec/http/requests/accessories_spec.rb
@@ -18,4 +18,26 @@ RSpec.describe 'GET /accessories' do
       expect(last_response.body).to eql('{"status":-70401}')
     end
   end
+
+  context 'Sufficient privileges and no error occurs' do
+    before do
+      set_cache(:controller_to_accessory_key, ['a' * 64].pack('H*'))
+      set_cache(:accessory_to_controller_key, ['b' * 64].pack('H*'))
+      get '/accessories', nil, {'CONTENT_TYPE' => 'application/hap+json'}
+    end
+
+    it 'headers contains application/hap+json' do
+      expect(last_response.headers).to include('Content-Type' => 'application/hap+json')
+    end
+
+    it 'response status 200' do
+      expect(last_response.status).to eql(200)
+    end
+
+    it 'responds with accessories' do
+      path = File.expand_path('../../../../lib/rubyhome/http/public/example_accessory.json', __FILE__)
+      data = File.read(path)
+      expect(last_response.body).to eql(data)
+    end
+  end
 end
