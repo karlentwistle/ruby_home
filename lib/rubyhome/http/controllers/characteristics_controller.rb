3c3dead4697b4114f6c3f04f0ebb8bd9136575c6
Karl Entwistle <karl@entwistle.com>
2018-03-03 18:53:19 +0000

    Add "hardcoded" characteristics response

--- lib/rubyhome/http/controllers/characteristics_controller.rb
+++ lib/rubyhome/http/controllers/characteristics_controller.rb
@@ -7,6 +7,20 @@ module Rubyhome
         content_type 'application/hap+json'
 
         if cache[:controller_to_accessory_key] && cache[:accessory_to_controller_key]
+          JSON.generate({
+            'characteristics' => [
+              {
+                'aid' => 2,
+                'iid' => 11,
+                'value' => false
+              },
+              {
+                'aid' => 2,
+                'iid' => 10,
+                'value' => 0
+              }
+            ]
+          })
         else
           status 401
           JSON.generate({"status" => -70401})
