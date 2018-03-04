ca6d4c54abdec12f768180eaf0109fcb6486ae32
Karl Entwistle <karl@entwistle.com>
2018-03-04 17:39:29 +0000

    Firmware Revision - Characteristic
    This characteristic describes a firmware revision string x[.y[.z]] (e.g. "100.1.1"):
    - <x> is the major version number, required.
    - <y> is the minor version number, required if it is non-zero or if <z> is present.
    - <z> is the revision version number, required if non-zero.
    
    The firmware revision must follow the below rules:
    - <x> is incremented when there is significant change. e.g.,1.0.0, 2.0.0, 3.0.0, etc.
    - <y> is incremented when minor changes are introduced such as 1.1.0, 2.1.0, 3.1.0 etc.
    - <z> is incremented when bug-fixes are introduced such as 1.0.1, 2.0.1, 3.0.1 etc.
    - Subsequent firmware updates can have a lower <y> version only if <x> is incremented Subsequent firmware updates can have a lower <z> version only if <x> or <y> is incremented

---
+++ spec/lib/hap/models/characteristics/firmware_revision_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require_relative '../../../../../lib/rubyhome/hap/models/characteristics/firmware_revision'
+
+RSpec.describe Rubyhome::Characteristic::FirmwareRevision, type: :model do
+  subject { described_class.new(value: '100.1.1') }
+
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a value' do
+      subject.value = nil
+      expect(subject).to_not be_valid
+    end
+  end
+end
