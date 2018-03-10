1af63ba63fb9a0b93283966950fec39a252d2db3
Karl Entwistle <karl@entwistle.com>
2018-03-10 16:17:19 +0000

    Persist data between server restarts

--- lib/rubyhome/accessory_info.rb
+++ lib/rubyhome/accessory_info.rb
@@ -6,59 +6,18 @@ module Rubyhome
   class AccessoryInfo
     PERSISTABLE = [ :device_id, :paired_clients, :password, :signature_key, :username ].freeze
 
-    @@accessory_info = AccessoryInfo.new
-
-    class << self
-      PERSISTABLE.each do |attribute|
-        define_method("#{attribute}=") do |value|
-          @@accessory_info.send("#{attribute}=", value)
-          save
-        end
-
-        define_method(attribute) do
-          @@accessory_info.send(attribute)
-        end
-      end
-
-      def save
-        @@accessory_info.save
-      end
-
-      def signing_key
-        @@accessory_info.signing_key
-      end
-
-      def paired?
-        paired_clients.any?
-      end
-
-      def add_paired_client(hash)
-        @@accessory_info.paired_clients << hash
-        save
-      end
-
-      def remove_paired_client(identifier)
-        @@accessory_info.paired_clients.delete_if { |h| h[:identifier] == identifier }
-        save
-      end
-    end
-
-    private_class_method :new
-
     def initialize
+      read
+
       @device_id ||= DeviceID.generate
       @paired_clients ||= []
-      @signature_key ||= Ed25519::SigningKey.generate.to_bytes
+      @signature_key ||= Ed25519::SigningKey.generate.to_bytes.unpack1('H*')
 
       save
     end
 
-    attr_reader :device_id, :signature_key, :paired_clients
-    attr_writer :device_id, :paired_clients
-
-    def signature_key=(signature_key)
-      @signature_key = signature_key
-    end
+    attr_reader :device_id, :signature_key, :paired_clients, :password, :username
+    attr_writer :device_id, :paired_clients, :password, :username
 
     def signature_key=(signature_key)
       @signing_key = nil
@@ -81,11 +40,60 @@ module Rubyhome
       Hash[*PERSISTABLE.flat_map { |attribute| [attribute, send(attribute)] }]
     end
 
-    def save
-      store = YAML::Store.new 'config.yml'
+    def read
+      store.transaction(true) do
+        store.fetch(:accessory_info, {}).each do |key, value|
+          send("#{key}=", value)
+        end
+      end
+    end
 
+    def save
       store.transaction do
         store[:accessory_info] = to_hash
+      end
+    end
+
+    def store
+      @store ||= YAML::Store.new 'config.yml'
+    end
+
+    @@accessory_info = AccessoryInfo.new
+
+    private_class_method :new
+
+    class << self
+      PERSISTABLE.each do |attribute|
+        define_method("#{attribute}=") do |value, save=true|
+          @@accessory_info.send("#{attribute}=", value)
+          save
+        end
+
+        define_method(attribute) do
+          @@accessory_info.send(attribute)
+        end
+      end
+
+      def save
+        @@accessory_info.save
+      end
+
+      def signing_key
+        @@accessory_info.signing_key
+      end
+
+      def paired?
+        @@accessory_info.paired_clients.any?
+      end
+
+      def add_paired_client(hash)
+        @@accessory_info.paired_clients << hash
+        save
+      end
+
+      def remove_paired_client(identifier)
+        @@accessory_info.paired_clients.delete_if { |h| h[:identifier] == identifier }
+        save
       end
     end
   end
