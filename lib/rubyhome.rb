43cdb91764775cc6b1857bdfb7be0dc5d758fbe3
Karl Entwistle <karl@entwistle.com>
2018-03-18 00:33:22 +0100

    Dynamically generate every factory apart from AccessoryInformation

--- lib/rubyhome.rb
+++ lib/rubyhome.rb
@@ -2,8 +2,6 @@ require_relative 'rubyhome/version'
 require_relative 'rubyhome/broadcast'
 require_relative 'rubyhome/identifier_cache'
 require_relative 'rubyhome/accessory_information_factory'
-require_relative 'rubyhome/fan_factory'
-
-Dir[File.dirname(__FILE__) + '/rubyhome/hap/builders/*.rb'].each { |file| require file }
+require_relative 'rubyhome/accessory_factory_generator'
 
 module Rubyhome; end
