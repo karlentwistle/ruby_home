24354065e01b44c89dd72a084e3ab01d4db696ad
Karl Entwistle <karl@entwistle.com>
2018-04-03 13:10:59 +0100

    Refactor CharacteristicSerializer implementation

--- lib/ruby_home/http/serializers/characteristic_serializer.rb
+++ lib/ruby_home/http/serializers/characteristic_serializer.rb
@@ -6,20 +6,27 @@ module RubyHome
       include ObjectSerializer
 
       def record_hash(characteristic)
-        record_hash = {}
+        {
+          'iid' => characteristic.instance_id,
+          'type' => characteristic.uuid,
+          'perms' => perms(characteristic),
+          'format' => characteristic.format,
+          'description' => characteristic.description,
+        }.merge(optional_hash(characteristic))
+      end
 
-        record_hash['iid'] = characteristic.instance_id
-        record_hash['type'] = characteristic.uuid
-        record_hash['perms'] = characteristic.properties.map do |property|
+      def perms(characteristic)
+        characteristic.properties.map do |property|
           RubyHome::Characteristic::PROPERTIES[property]
         end.compact
-        record_hash['format'] = characteristic.format
-        if characteristic.value != nil
-          record_hash['value'] = characteristic.value
-        end
-        record_hash['description'] = characteristic.description
+      end
 
-        record_hash
+      def optional_hash(characteristic)
+        Hash.new.tap do |optional_hash|
+          if characteristic.value != nil
+            optional_hash['value'] = characteristic.value
+          end
+        end
       end
     end
   end
