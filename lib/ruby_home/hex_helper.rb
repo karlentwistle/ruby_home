ae3623d4188c9fe269e8365c687ae232262859b6
Karl Entwistle <karl@entwistle.com>
2018-07-23 20:10:25 +0100

    Update method signatures

--- lib/ruby_home/hex_helper.rb
+++ lib/ruby_home/hex_helper.rb
@@ -1,14 +1,14 @@
 module RubyHome
   module HexHelper
     def self.pad(input, pad_length: 24)
-      unpack_hex_string(pack_hex_string(input).rjust(pad_length, '0'))
-    end
-
-    def self.pack_hex_string(input)
-      input.to_s.unpack1('H*')
+      pack_hex_string(unpack_hex_string(input).rjust(pad_length, '0'))
     end
 
     def self.unpack_hex_string(input)
+      input.to_s.unpack1('H*')
+    end
+
+    def self.pack_hex_string(input)
       [input].pack('H*')
     end
   end
