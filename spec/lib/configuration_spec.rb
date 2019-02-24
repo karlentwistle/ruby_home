a2b1dff40893e9040d60dbab2f7623816639f5f6
Karl Entwistle <karl@entwistle.com>
2019-02-24 19:04:45 +0000

    Namespace specifications inside describe block

--- spec/lib/configuration_spec.rb
+++ spec/lib/configuration_spec.rb
@@ -4,23 +4,27 @@ RSpec.describe RubyHome::Configuration do
   let(:accessory_info) { RubyHome::AccessoryInfo.instance }
   subject(:configuration) { described_class.new }
 
-  it 'allows configuration of model_name' do
-    configuration.model_name = 'Custom Model Name'
-    expect(configuration.model_name).to eql('Custom Model Name')
+  describe '#model_name' do
+    it 'allows configuration of model_name' do
+      configuration.model_name = 'Custom Model Name'
+      expect(configuration.model_name).to eql('Custom Model Name')
+    end
+
+    it 'sets model_name within accessory info' do
+      configuration.model_name = 'Custom Model Name'
+      expect(accessory_info.model_name).to eql('Custom Model Name')
+    end
   end
 
-  it 'sets model_name within accessory info' do
-    configuration.model_name = 'Custom Model Name'
-    expect(accessory_info.model_name).to eql('Custom Model Name')
-  end
+  describe "#password" do
+    it 'allows configuration of password' do
+      configuration.password = '031-45-154'
+      expect(configuration.password).to eql('031-45-154')
+    end
 
-  it 'allows configuration of password' do
-    configuration.password = '031-45-154'
-    expect(configuration.password).to eql('031-45-154')
-  end
-
-  it 'sets password within accessory info' do
-    configuration.password = '031-45-154'
-    expect(accessory_info.password).to eql('031-45-154')
+    it 'sets password within accessory info' do
+      configuration.password = '031-45-154'
+      expect(accessory_info.password).to eql('031-45-154')
+    end
   end
 end
