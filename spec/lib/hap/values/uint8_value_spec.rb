46a5c25f04299a94b4fb436e60cd8245ea22be55
Rob Cameron <cannikinn@gmail.com>
2019-06-07 11:10:36 -0700

    Adds spec for percentage unit and unknown unit

--- spec/lib/hap/values/uint8_value_spec.rb
+++ spec/lib/hap/values/uint8_value_spec.rb
@@ -2,10 +2,28 @@ require 'spec_helper'
 
 RSpec.describe RubyHome::Uint8Value do
   describe '#default' do
-    it 'returns first default value from valid values' do
-      template = double(constraints: { 'ValidValues' => { '0' => 'Inactive' } } )
-      handler = RubyHome::Uint8Value.new(template)
-      expect(handler.default).to eql(0)
+    context 'unit nil' do
+      it 'returns first default value from ValidValues' do
+        template = double(constraints: { 'ValidValues' => { '0' => 'Inactive' } } )
+        handler = RubyHome::Uint8Value.new(template)
+        expect(handler.default).to eql(0)
+      end
+    end
+
+    context 'unit percentage' do
+      it 'returns first default value MinimumValue' do
+        template = double(constraints: { 'MaximumValue' => 100, 'MinimumValue' => 0, 'StepValue' => 1} )
+        handler = RubyHome::Uint8Value.new(template)
+        expect(handler.default).to eql(0)
+      end
+    end
+
+    context 'unit unknown' do
+      it 'raises an error' do
+        template = double(constraints: { 'UnknownValue' => 0 } )
+        handler = RubyHome::Uint8Value.new(template)
+        expect { handler.default }.to raise_error(RubyHome::UnknownValueError)
+      end
     end
   end
 end
