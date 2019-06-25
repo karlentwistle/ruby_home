afc90e4659bc53010db68778c2bb91c657134fe5
Karl Entwistle <karl@entwistle.com>
2019-06-25 19:59:12 +0100

    dont attempt to clear up files after test suite has finished running (#72)

--- spec/support/identifier_cache.rb
+++ spec/support/identifier_cache.rb
@@ -1,16 +1,13 @@
 RSpec.configure do |config|
   config.before(:suite) do
+    if File.exists?(RubyHome::IdentifierCache.source)
+      File.delete(RubyHome::IdentifierCache.source)
+    end
     source = File.join(File.dirname(__FILE__), '/../tmp/identifier_cache.yml')
     RubyHome::IdentifierCache.source = source
   end
 
   config.after(:each) do
     RubyHome::IdentifierCache.truncate
-  end
-
-  config.after(:suite) do
-    if File.exists?(RubyHome::IdentifierCache.source)
-      File.delete(RubyHome::IdentifierCache.source)
-    end
   end
 end
