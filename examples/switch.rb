a021b5af1f06542030c262585170a7cc1975976e
Karl Entwistle <karl@entwistle.com>
2019-06-26 19:18:06 +0100

    Add switch example

---
+++ examples/switch.rb
@@ -0,0 +1,17 @@
+require 'ruby_home'
+
+accessory_information = RubyHome::ServiceFactory.create(:accessory_information)
+switch = RubyHome::ServiceFactory.create(:switch,
+  on: false, # required
+  name: 'switch', # optional
+)
+
+switch.on.after_update do |on|
+  if on
+    puts "switch is on"
+  else
+    puts "switch is off"
+  end
+end
+
+RubyHome.run
