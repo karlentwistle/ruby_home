689bb4e71395f9142103b1a32d2297e87532bb0f
Karl Entwistle <karl@entwistle.com>
2019-09-25 20:24:15 +0100

    Add Color Temperature characteristic to Lightbulb service (#80)
    For some reason Apple doesn't appear to have included "Color Temperature" as a
    characteristic of the "Lightbulb" service in within HAPAccessoryKit.framework
    Resources default.metadata.plist
    
    I've tweaked the characteristic and services generators to manually include
    
    - Color temperature as an optional characteristic of the lightbulb service
    - Color temperature as a characteristic
    
    Fixes #78

--- sbin/characteristic_generator.rb
+++ sbin/characteristic_generator.rb
@@ -10,8 +10,12 @@ module RubyHome
       def run
         convert_binary_plist
         File.open(file_path, 'w') do |file|
-          file.write generate_characteristics.to_yaml
+          file.write characteristics.to_yaml
         end
+      end
+
+      def characteristics
+        generate_characteristics + hardcoded_characteristics
       end
 
       def generate_characteristics
@@ -19,6 +23,24 @@ module RubyHome
           new(characteristics_xml).generate_characteristic_hash
         end
       end
+
+      def hardcoded_characteristics
+        [
+          # HomeKit Accessory Simulator doesnt include color temperature as a characteristic
+          # HAP-Specification-Non-Commercial-Version lists it as a valid optional characteristic
+          {
+            name: :color_temperature,
+            description: 'Color Temperature',
+            uuid: '000000CE-0000-1000-8000-0026BB765291',
+            format: 'uint32',
+            unit: "nil",
+            permissions: %w(securedRead securedWrite),
+            properties: %w(read write cnotify),
+            constraints: { 'MaximumValue': 400, 'MinimumValue': 50, 'StepValue': 1 }
+          }
+        ]
+      end
+
 
       def convert_binary_plist
         system([
