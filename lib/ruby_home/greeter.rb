59b82427d181804fe2d7eca2663d06d4419c050a
erpel <philipp@copythat.de>
2020-12-23 17:06:40 +0100

    Add QR code output to greeter

--- lib/ruby_home/greeter.rb
+++ lib/ruby_home/greeter.rb
@@ -1,3 +1,4 @@
+require 'rqrcode'
 module RubyHome
   module Greeter
     class << self
@@ -9,6 +10,7 @@ module RubyHome
           puts "    │ " + pin + " │    "
           puts "    └────────────┘     "
           puts "                       "
+          puts ascii_qrcode
         end
       end
 
@@ -22,6 +24,41 @@ module RubyHome
 
       def accessory_info
         AccessoryInfo.instance
+      end
+
+      def url
+        record = RubyHome.dns_service.text_record
+        category = record.accessory_category_identifier
+        password = accessory_info.password
+        setup_id = accessory_info.setup_id
+
+        version = 0
+        reserved = 0
+        flags = record.feature_flags
+
+        payload = 0
+        payload |= (version & 0x7)
+
+        payload <<= 4
+        payload |= (reserved & 0xf)
+
+        payload <<= 8
+        payload |= (category & 0xff)
+
+        payload <<= 4
+        payload |= (flags & 0xf)
+
+        payload <<= 27
+        payload |= password.gsub('-', '').to_i(10) & 0x7fffffff
+
+        "X-HM://#{payload.to_s(36).rjust(9, '0').upcase}#{setup_id.upcase}"
+      end
+
+      def ascii_qrcode
+        qrcode = RQRCode::QRCode.new(url)
+        terminal_qr = qrcode.to_s
+        terminal_qr.gsub!(' ', '⬜')
+        terminal_qr.gsub!('x', '⬛')
       end
     end
   end
