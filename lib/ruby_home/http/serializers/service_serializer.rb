215953701b86e9a5cc07b8e0929cd1c87c15f170
Karl Entwistle <me@karlentwistle.com>
2021-01-13 20:55:36 +0000

    Fix failing spec

--- lib/ruby_home/http/serializers/service_serializer.rb
+++ lib/ruby_home/http/serializers/service_serializer.rb
@@ -9,18 +9,14 @@ module RubyHome
       include UUIDHelper
 
       def record_hash(service)
-        characteristics = service.characteristics.select do |c|
-          c.properties.include?("read") || !c.valid_values.nil?
-        end
-        
         result = {
           'iid' => service.instance_id,
           'type' => uuid_short_form(service.uuid),
-          'characteristics' => CharacteristicSerializer.new(characteristics).serializable_hash,
+          'characteristics' => CharacteristicSerializer.new(service.characteristics).serializable_hash,
           'primary' => service.primary,
           'hidden' => service.hidden,
         }
-        
+
         linked = service.linked
         if linked.nil? || linked.empty?
           result
